80e8f222a74a5f2d3e0310b41d0014fb5d4da818
bonnieli1414 <87414209+bonnieli1414@users.noreply.github.com>
2023-05-10 15:58:09 +0800

    新增回到頂端

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// 滾輪條大於0觸發，改變header背景顏色
-function changeHeaderBgColor() {
-    $(window).scrollTop() > 0 ?
-        $('header').addClass('bg-light').addClass('shadow-c') : $('header').removeClass('bg-light').removeClass('shadow-c')
+// 回到頂端
+const toTopFn = () => {
+    $('html, body').scrollTop(0);
 }
-// changeHeaderBgColor()
+$('.to-top').on('click', toTopFn)

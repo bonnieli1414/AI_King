c6ac8570370ad0433e295b2fc13a6236ca43acc6
bonnieli1414 <87414209+bonnieli1414@users.noreply.github.com>
2023-05-11 16:29:27 +0800

    更新

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const toTopFn = () => {
     $('html, body').scrollTop(0);
 }
 $('.to-top').on('click', toTopFn)
+
+// 動態往上
+// $('.animation img')
+
+// const animationToUp = () => {
+    
+// }
+// setIntervalId = setInterval(animationToUp, 1000);

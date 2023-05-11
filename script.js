ba1ffe942cab34ca7b094b2b14e66eb29227298e
bonnieli1414 <87414209+bonnieli1414@users.noreply.github.com>
2023-05-11 18:02:30 +0800

    更新

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ $('.to-top').on('click', toTopFn)
 
 // 動態往上
 let toUpIndex = 0;
-const distance = ['-200px', '0px', '0px', '200px'];
+const distance = ['-200px', '-10px', '0px', '200px', '210px'];
 const animationToUp = (dom) => {
     dom.css('bottom', distance[toUpIndex % distance.length])
-toUpIndex++;
-
+    toUpIndex++;
+    console.log(distance[toUpIndex % distance.length])
+    if (distance[toUpIndex % distance.length]==="-200px" || distance[toUpIndex % distance.length]==="-10px"){
+        dom.css('opacity', 0)
+    }else {
+        dom.css('opacity', 1)
+    }
 }
 setIntervalId01 = setInterval(animationToUp, 1000, $($('.animation img')[0]));
 setIntervalId02 = setInterval(animationToUp, 1000, $($('.animation img')[1]));

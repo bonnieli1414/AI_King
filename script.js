5bd2ac06279cdccb910beb8aef00a09658322ba1
bonnieli1414 <87414209+bonnieli1414@users.noreply.github.com>
2023-05-16 22:40:05 +0800

    更新

--- script.js
+++ script.js
@@ -6,17 +6,25 @@ $('.to-top').on('click', toTopFn)
 
 // 動態往上
 let toUpIndex = 0;
-const distance = ['-200px', '-10px', '0px', '200px', '210px'];
-const animationToUp = (dom) => {
-    dom.css('bottom', distance[toUpIndex % distance.length])
+const distance = ['-200px', '0px', '0px', '200px', '200px', '400px'];
+const animationToUp = (dom, index) => {
+    // console.log(distance[toUpIndex], index)
+    dom.css('bottom', distance[toUpIndex])
     toUpIndex++;
-    console.log(distance[toUpIndex % distance.length])
-    if (distance[toUpIndex % distance.length]==="-200px" || distance[toUpIndex % distance.length]==="-10px"){
-        dom.css('opacity', 0)
-    }else {
-        dom.css('opacity', 1)
+    if (distance[toUpIndex] >= '400px') {
+        dom.css('display', 'none')
+        toUpIndex = 0
+    } else if (distance[toUpIndex] === '-200px') {
+        dom.css('display', 'none')
+    } else {
+        dom.css('display', 'block')
     }
 }
-setIntervalId01 = setInterval(animationToUp, 1000, $($('.animation img')[0]));
-setIntervalId02 = setInterval(animationToUp, 1000, $($('.animation img')[1]));
-setIntervalId03 = setInterval(animationToUp, 1000, $($('.animation img')[2]));
+
+const animationImg = $('.animation img')
+animationImg.each((index, element) => {
+    setInterval(animationToUp, 1000, $(animationImg[element]), index)
+});
+setIntervalId01 = setInterval(animationToUp, 1000, $($('.animation img')[0]), 1);
+setIntervalId02 = setInterval(animationToUp, 1000, $($('.animation img')[1]), 2);
+setIntervalId03 = setInterval(animationToUp, 1000, $($('.animation img')[2]), 3);

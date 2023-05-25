f6ef791bdf15e694b6ffff00c0406809539e9693
bonnieli1414 <bonnieli1414@gmail.com>
2023-05-25 22:17:45 +0800

    來自合作夥伴套用swipper套件

--- script.js
+++ script.js
@@ -4,28 +4,8 @@ const toTopFn = () => {
 }
 $('.to-top').on('click', toTopFn)
 
-// // 動態往上
-// let toUpIndex = 0;
-// const distance = ['-200px', '0px', '0px', '200px', '200px', '400px'];
-// const animationToUp = (dom) => {
-//     // console.log(distance[toUpIndex], index)
-//     dom.css('bottom', distance[toUpIndex])
-//     toUpIndex++;
-//     if (distance[toUpIndex] >= '400px') {
-//         dom.css('display', 'none')
-//         toUpIndex = 0
-//     } else if (distance[toUpIndex] === '-200px') {
-//         dom.css('display', 'none')
-//     } else {
-//         dom.css('display', 'block')
-//     }
-// }
-
-// const animationImg = $('.animation img')
-// animationImg.each((index, element) => {
-//     console.log($(animationImg[index]))
-//     setInterval(animationToUp, 1000, $(animationImg[index]))
-// });
-// setIntervalId01 = setInterval(animationToUp, 1000, $(animationImg[0]));
-// setIntervalId02 = setInterval(animationToUp, 1000, $(animationImg[1]));
-// setIntervalId03 = setInterval(animationToUp, 1000, $(animationImg[2]));
+// 引入swiper套件
+var swiper = new Swiper(".mySwiper", {
+    watchSlidesProgress: true,
+    slidesPerView: 5,
+});
